b3872bec2cddab0722cd174808351861e1c9b3f9
max990624 <max990624@bible.ac.kr>
2022-05-30 04:31:25 +0900

    주석 추가

--- observer.js
+++ observer.js
@@ -1,8 +1,6 @@
-function Subject() {
+function Subject() { // Subject
     this.observers = [];
 }
-
-Subject.prototype = {}
 Subject.prototype.attachObserver = function(observer) {
         this.observers.push(observer);
 }
@@ -13,21 +11,20 @@ Subject.prototype.notifyObservers = function(data) {
         this.observers.forEach(observer => observer.update(data));
 }
 
-function Observer(){
-    this.update = function(data){ //update 구현된 부분이 파리 prototype에서 구현되어야함
-        console.log('%s에게 데미지 : %d', this.name, data)
-        console.log('파리 %s의 체력 : %d\n', (this.name), (this.hp + data));
-        this.hp = this.hp + data
-    }
+function Observer() {} // Observer
+Observer.prototype.update = function (data) { //update 구현된 부분이 파리 prototype에서 구현되어야함
+    console.log('%s에게 데미지 : %d', this.name, data)
+    console.log('%s의 체력 : %d\n', (this.name), (this.hp + data));
+    this.hp = this.hp + data
 }
 
-const subject = new Subject();
+const subject = new Subject(); // ConcreteSubject
 
-const observer1 = new Observer();
+const observer1 = new Observer(); // ConcreteObserver 1
 observer1.hp = 50
 observer1.name = "바알제붑"
 
-const observer2 = new Observer();
+const observer2 = new Observer(); // ConcreteObserver 2
 observer2.hp = 45
 observer2.name = "꿀빠는 꿀벌"
 
